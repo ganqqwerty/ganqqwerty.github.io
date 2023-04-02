477b7641bf28dbe718c962676ff4a0da21f3741a
Yuri Katkov <katkov.juriy@gmail.com>
2023-04-03 02:53:52 +0900

    Update katakana.js

--- katakana.js
+++ katakana.js
@@ -1,78 +1,68 @@
-const URL = "loanwords.json";
+const appDiv = document.getElementById('app');
 
-async function getLoanwords() {
-    try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-            throw new Error("Network response was not ok.");
-        }
-        const loanwords = await response.json();
-        if (!isValidLoanwords(loanwords)) {
-            throw new Error("Loanwords JSON file is not in the correct format.");
-        }
-        return loanwords;
-    } catch (error) {
-        console.error(`Error fetching loanwords: ${error}`);
-        return [];
-    }
+function isValidLoanword(loanword) {
+  return loanword.hiragana && loanword.katakana && loanword.romaji && loanword.translation;
 }
 
-function isValidLoanwords(loanwords) {
-    if (!Array.isArray(loanwords)) {
-        return false;
-    }
-    const requiredFields = ["hiragana", "katakana", "romaji", "translation"];
-    for (const loanword of loanwords) {
-        if (typeof loanword !== "object") {
-            return false;
-        }
-        for (const field of requiredFields) {
-            if (!(field in loanword)) {
-                return false;
-            }
-        }
-    }
-    return true;
+function generateTableRow(loanword) {
+  const hiraganaCell = document.createElement('td');
+  hiraganaCell.textContent = loanword.hiragana;
+  hiraganaCell.classList.add('hiragana');
+
+  const katakanaCell = document.createElement('td');
+  katakanaCell.textContent = loanword.katakana;
+
+  const romajiCell = document.createElement('td');
+  romajiCell.textContent = loanword.romaji;
+  romajiCell.classList.add('romaji');
+
+  const translationCell = document.createElement('td');
+  translationCell.textContent = loanword.translation;
+  translationCell.classList.add('translation');
+
+  const row = document.createElement('tr');
+  row.appendChild(hiraganaCell);
+  row.appendChild(katakanaCell);
+  row.appendChild(romajiCell);
+  row.appendChild(translationCell);
+
+  return row;
 }
 
 function generateTable(loanwords) {
-    const table = document.createElement("table");
-    const headers = ["Katakana", "Hiragana", "Romaji", "Translation"];
-    table.appendChild(generateHeaderRow(headers));
-    table.append(...loanwords.map(loanword => generateRow(loanword, headers)));
-    return table;
+  const table = document.createElement('table');
+  table.id = 'loanwords-table';
+
+  const headerRow = document.createElement('tr');
+  const hiraganaHeader = document.createElement('th');
+  hiraganaHeader.textContent = 'Hiragana';
+  const katakanaHeader = document.createElement('th');
+  katakanaHeader.textContent = 'Katakana';
+  const romajiHeader = document.createElement('th');
+  romajiHeader.textContent = 'Romaji';
+  const translationHeader = document.createElement('th');
+  translationHeader.textContent = 'Translation';
+
+  headerRow.appendChild(hiraganaHeader);
+  headerRow.appendChild(katakanaHeader);
+  headerRow.appendChild(romajiHeader);
+  headerRow.appendChild(translationHeader);
+  table.appendChild(headerRow);
+
+  const tableRows = loanwords.filter(isValidLoanword).map(generateTableRow);
+  tableRows.forEach(row => table.appendChild(row));
+
+  return table;
 }
 
-function generateHeaderRow(headers) {
-    const headerRow = document.createElement("tr");
-    headerRow.append(...headers.map(header => generateHeader(header)));
-    return headerRow;
+function loadData() {
+  fetch('loanwords.json')
+    .then(response => response.json())
+    .then(data => {
+      const table = generateTable(data.loanwords);
+      appDiv.appendChild(table);
+    })
+    .catch(error => console.error('Error loading loanwords', error));
 }
 
-function generateHeader(header) {
-    const th = document.createElement("th");
-    th.textContent = header;
-    return th;
-}
-
-function generateRow(loanword, headers) {
-    const row = document.createElement("tr");
-    row.append(...headers.map(header => generateCell(loanword[header])));
-    return row;
-}
-
-function generateCell(text) {
-    const td = document.createElement("td");
-    td.textContent = text;
-    return td;
-}
-
-async function main() {
-    const loanwords = await getLoanwords();
-    const table = generateTable(loanwords);
-    document.getElementById("app").appendChild(table);
-}
-
-main().catch(error => {
-    console.error(error);
-});
+loadData();
